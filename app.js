ffedcc7de7168b83ae795ce38a221613e7fdb380
Muhammad Yusuf Maulana <muhammad.yusuf@rcl.co.id>
2017-08-29 18:47:50 +0700

    [TASK-0] initial commit with JWT auth integration

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+
+var authenticateController=require('./controllers/authenticate-controller');
+process.env.SECRET_KEY="thisismysecretkey";
+
+var app = express();
+var router=express.Router();
+var jwt= require("jsonwebtoken");
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+
+app.post('/api/authenticate',authenticateController.authenticate);
+app.use('/secure-api',router);
+
+// validation middleware
+var router=express.Router();
+router.use(function(req,res,next){
+    var token=req.body.token || req.headers['token'];
+    if(token){
+        jwt.verify(token,process.env.SECRET_KEY,function(err,ress){
+            if(err){
+                res.status(500).send('Token Invalid');
+            }else{
+                next();
+            }
+        })
+    }else{
+        res.send('Please send a token')
+    }
+})
+router.get('/home',function(req,res){
+    res.send('Token Verified')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/authenticate-controller.js
@@ -0,0 +1,38 @@
+var jwt=require('jsonwebtoken');
+var connection = require('./../config/mysql');
+module.exports.authenticate=function(req,res){
+    var email=req.body.email;
+    var password=req.body.password;
+    connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+      if (error) {
+          res.json({
+            status:false,
+            message:'there are some error with query'
+            })
+      }else{  
+        if(results.length >0){
+            if(password==results[0].password){
+                var token=jwt.sign(results[0],process.env.SECRET_KEY,{
+                    expiresIn:5000
+                });
+                res.json({
+                    status:true,
+                    token:token
+                })
+            }else{
+                res.json({
+                  status:false,                  
+                  message:"Email and password does not match"
+                 });
+            }
+         
+        }
+        else{
+          res.json({
+              status:false,
+            message:"Email does not exits"
+          });
+        }
+      }
+    });
+}
